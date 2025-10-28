155bb94660bfd71ac24df86466dc6cfddb1913e5
Nhân <nhankotu@gmail.com>
2025-10-28 11:21:32 +0700

    Fix Vite config for production deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // quan trọng
+      "@": path.resolve(__dirname, "src"),
     },
+  },
+
+  base: "./",
+  build: {
+    outDir: "dist",
   },
 });
