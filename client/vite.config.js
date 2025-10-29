ef26dde9e4789581c4f745ac604d2d0687d2be83
Nhân <nhankotu@gmail.com>
2025-10-29 21:28:05 +0700

    add vercel.json for spa routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-
-  base: "./",
+  base: "/", // ✅ QUAN TRỌNG: phải là "/"
   build: {
     outDir: "dist",
   },
