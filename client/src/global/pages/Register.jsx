74e1ce3136c1ee856a64a72e2f8e5410fa4b6f86
Nhân <nhankotu@gmail.com>
2025-10-29 20:20:05 +0700

    sua duong dan Api

--- client/src/global/pages/Register.jsx
+++ client/src/global/pages/Register.jsx
@@ -103,7 +103,7 @@ function Register() {
   // Gửi lại OTP
   const handleResendOTP = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/users/register", {
+      const res = await fetch(`${API_BASE}/api/users/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
